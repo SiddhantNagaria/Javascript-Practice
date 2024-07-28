fbe32c84d960aba91760198bdd4f3f964e730d1e
Siddhant Nagaria <98863312+SiddhantNagaria@users.noreply.github.com>
2024-07-28 15:14:17 +0000

    datatypes

---
+++ 1_Basics/1.variables.js
@@ -0,0 +1,21 @@
+const account = 123
+let email = "sid@abc.com"
+var accPass = "1234"
+accCity = "Jhansi"
+let accState;
+
+// account = 456 
+
+email = "sidddd@qwe.coom"
+accPass = "5678"
+accCity="Delhi"
+
+console.log(account);
+console.table([account, email, accPass, accCity, accState])
+
+//var use nahi karte hai
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
