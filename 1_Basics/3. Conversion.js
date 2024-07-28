b6ba64ca764509b00e98583c01cfc53eecc97e63
Siddhant Nagaria <98863312+SiddhantNagaria@users.noreply.github.com>
2024-07-28 15:25:16 +0000

    conversion Data Types

---
+++ 1_Basics/3. Conversion.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof score);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1 false => 0
+
+let isLoggedIn = "siddhant"
+let booleanlogin = Boolean(isLoggedIn)
+console.log(booleanlogin);
+
+//1=>true , 0=>false
+
+// "" => false
+// "siddhant" => true
+
+let someNumber = 33
+let stringNum = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNum);
