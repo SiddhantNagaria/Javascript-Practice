f38125f83f4ad3d5f437b9ca8e7df18d0ceeec81
Siddhant Nagaria <98863312+SiddhantNagaria@users.noreply.github.com>
2024-07-29 06:48:21 +0000

    conversion

--- 1_Basics/3. Conversion.js
+++ 1_Basics/3. Conversion.js
@@ -1,27 +1,64 @@
-let score = true
+// let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(typeof score);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(typeof score);
+// console.log(valueInNumber);
 
-//"33" => 33
-// "33abc" => NaN
-//true => 1 false => 0
+// //"33" => 33
+// // "33abc" => NaN
+// //true => 1 false => 0
 
-let isLoggedIn = "siddhant"
-let booleanlogin = Boolean(isLoggedIn)
-console.log(booleanlogin);
+// let isLoggedIn = "siddhant"
+// let booleanlogin = Boolean(isLoggedIn)
+// console.log(booleanlogin);
 
-//1=>true , 0=>false
+// //1=>true , 0=>false
 
-// "" => false
-// "siddhant" => true
+// // "" => false
+// // "siddhant" => true
 
-let someNumber = 33
-let stringNum = String(someNumber)
-console.log(someNumber)
-console.log(typeof stringNum);
+// let someNumber = 33
+// let stringNum = String(someNumber)
+// console.log(someNumber)
+// console.log(typeof stringNum);
+
+
+// //**************************   Operations  ******************* 
+
+let value = 3
+let negval = -value
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "siddhant"
+let str2 = " nagaria"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 *5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let counter = 100
+counter++;
+console.log(counter);
