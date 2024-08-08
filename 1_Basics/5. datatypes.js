a8a43b21c28c00a7fef5214f20b13847d56ac4ad
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-08 19:04:36 +0530

    data type summary

---
+++ 1_Basics/5. datatypes.js
@@ -0,0 +1,31 @@
+const score = 100
+const scoreVal = 100.3
+
+const isLoggedIn = false;
+const temp = null;
+let userEmail;
+
+const id = Symbol('123');
+const AnotherId = Symbol('123');
+
+console.log(id == AnotherId);
+
+const bigNumber = 34534565475675765756376n
+
+const hero = ["AK", "M416", "P90"];
+
+let obj = {
+    name: "John",
+    age: 30
+}
+
+const myFunc = function(){
+    console.log("hello dear");
+    
+}
+myFunc();
+
+console.log(typeof bigNumber);
+console.log(typeof myFunc);
+
+
