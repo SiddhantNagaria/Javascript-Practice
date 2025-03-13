a94343ae8d32b7402d7e44e6b732c62f43460342
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-13 21:23:22 +0530

    object literals

---
+++ 3_Object_Literals/Object_literals.js
@@ -0,0 +1,21 @@
+let student ={
+    name: "John",
+    age: 20,
+    grade: 90
+};
+
+const student2 ={
+    name: "John",
+    age: 20,
+    grade: 90
+};
+
+const item = {
+    price:200,
+    disount:50,
+    colors: ["red", "pink"]
+};
+
+console.log(student);
+console.log(student2);
+console.log(item);
