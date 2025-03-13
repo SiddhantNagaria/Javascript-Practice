50f05d6eb1598fd07c866f4f4c3e5835ed7c43fa
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-13 21:31:30 +0530

    create object

---
+++ 3_Object_Literals/create a post.js
@@ -0,0 +1,16 @@
+// create an object literal for the properties of thread/twitter
+// post which includes - 
+// useranme
+// content
+// likes
+// retweets
+// comments
+
+
+const post = {
+    username: "siddhant",
+    content: "hello world",
+    likes: 0,
+    retweets: 0,
+    comments: []
+};
