cd8efba5827053f39d74f3202667f462db65b231
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-13 21:38:27 +0530

    get valued of object

---
+++ 3_Object_Literals/Get_values.js
@@ -0,0 +1,11 @@
+let student = {
+    name:"siddhant",
+    age:20,
+    grade:90
+};
+
+console.log(student.name);
+console.log(student["name"]);
+
+console.log(student.age);
+console.log(student["age"]);
