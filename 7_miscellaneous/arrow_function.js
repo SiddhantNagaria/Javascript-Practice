bcee69492c74c52e267f26c8f3cee1320944bc90
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-20 00:04:13 +0530

    arrow functionss

---
+++ 7_miscellaneous/arrow_function.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => {
+  return a + b;
+};
+console.log(sum(5, 7)); // 12
+
+const cube = (n) => {
+  return n * n * n;
+};
+console.log(cube(3)); //27
+
+
+const pow = (a,b)=>{
+    return Math.pow(a,b);
+}
+console.log(pow(2,3)); //8
