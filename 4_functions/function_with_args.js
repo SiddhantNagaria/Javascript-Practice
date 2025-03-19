5b86a854e9442441ee780c892ef867e3380bb92b
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-19 08:12:46 +0530

    function with args

---
+++ 4_functions/function_with_args.js
@@ -0,0 +1,9 @@
+function add(num1,num2){
+    console.log(num1+num2);
+};
+add(10,20);
+
+function printINfo(name,age){
+    console.log(`my name is ${name} and my age is ${age}`);
+}
+printINfo('John',30);
