95f1aeca4dc5f1d5c409aab5502b768c4768fb53
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-19 21:55:06 +0530

    methods in js

---
+++ 6_functions_2/methods.js
@@ -0,0 +1,15 @@
+// actions performed on an object 
+const calculator = {
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+    divide: function(a,b){
+        return a/b;
+    }
+}
