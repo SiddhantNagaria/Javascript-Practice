35dad7e1ea2e1e47bfdf5d4cea86fc330247e485
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-19 22:00:33 +0530

    methods-shorthand

--- 6_functions_2/methods.js
+++ 6_functions_2/methods.js
@@ -13,3 +13,19 @@ const calculator = {
         return a/b;
     }
 }
+
+//methods-shorthand
+const calculator2 = {
+    add(a,b){
+    return a+b;
+    },
+    sub(a,b){
+    return a-b;
+    },
+    multiply(a,b){
+    return a*b;
+    },
+    divide(a,b){
+    return a/b;
+    }
+};
