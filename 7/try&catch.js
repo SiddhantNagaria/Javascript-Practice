09edfafccb2ec0cd0ebd5c7f3a857e8dd2a70e45
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-19 23:48:12 +0530

    try catch

---
+++ 7/try&catch.js
@@ -0,0 +1,13 @@
+try{
+    console.log(a);
+}catch{
+    console.log("a is not defined");
+}
+
+/////////////////////////////////////////
+
+try{
+    console.log(a);
+}catch(e){
+    console.log(e);
+}
