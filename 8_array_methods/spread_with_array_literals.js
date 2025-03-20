a118cd9f0bb3e41fed5d1fd8830c61831235fa83
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-21 00:00:22 +0530

    spread on literals

---
+++ 8_array_methods/spread_with_array_literals.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4,5];
+let newArr= [...arr];
+console.log(newArr);
+
+let chars=[..."hello world"];
+console.log(chars);
+
---
+++ 8_array_methods/spread_with_object_literals.js
@@ -0,0 +1,8 @@
+let data= {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+};
+
+let datacopy = {...data, id:123};
+console.log(datacopy);
