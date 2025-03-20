51047c98db14afef2c05a4323d07651104220299
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-21 00:27:03 +0530

    de-sctructuring

---
+++ 8_array_methods/de-structuring.js
@@ -0,0 +1,5 @@
+let names = ["sid", "siddhant", "nagaria"];
+
+let [a, b, c] = names;
+console.log(a, b);
+console.log(names);
---
+++ 8_array_methods/de-structuring_objects.js
@@ -0,0 +1,9 @@
+const student = {
+  name: "siddhant",
+  age: 20,
+  grade: 90,
+};
+
+let { name: firstname, age: dob } = student;
+console.log(student);
+console.log(firstname);
