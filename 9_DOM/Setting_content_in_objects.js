f4bfdd6f0c78eb4a1081d473a2a278049b788ecc
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-21 18:57:50 +0530

    DOM manipulation

---
+++ 9_DOM/Setting_content_in_objects.js
@@ -0,0 +1,12 @@
+document.getElementById("id").innerHTML = "Hello";
+document.getElementById("id").innerText = "Hello";
+document.getElementById("id").textContent = "Hello";
+
+document.getElementById("id").innerHTML = "<h1>Hello</h1>";
+document.getElementById("id").innerText = "<h1>Hello</h1>";
+document.getElementById("id").textContent = "<h1>Hello</h1>";
+
+document.getElementById("id").innerHTML = "<h1>Hello</h1><h2>Hello</h2>";
+document.getElementById("id").innerText = "<h1>Hello</h1><h2>Hello</h2>";
+document.getElementById("id").textContent = "<h1>Hello</h1><h2>Hello</h2>";
+
