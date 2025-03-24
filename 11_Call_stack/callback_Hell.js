10d638d6488a0d17e72fb598d4cda1f588e6216a
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-25 00:55:13 +0530

    callback hell

---
+++ 11_Call_stack/callback_Hell.js
@@ -0,0 +1,18 @@
+h1 = document.querySelector("h1");
+
+function changecolor(color, delay, nextColor) {
+  setTimeout(() => {
+    h1.style.color = color;
+    if (nextColor) {
+        changecolor();
+    }
+  }, delay);
+}
+
+changecolor("red", 1000, () => {
+  changecolor("blue", 1000, () => {
+    changecolor("green", 1000);
+  });
+});
+
+// callback hell -> callback nesting
