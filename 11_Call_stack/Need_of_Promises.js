b76307d3e46ae1b549b63e4885583b158d1de738
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-25 01:34:46 +0530

    need of promises

--- 11_Call_stack/Need_of_Promises.js
+++ 11_Call_stack/Need_of_Promises.js
@@ -25,3 +25,10 @@ savetoDb(
     console.log("failure: unable to save data");
   }
 );
+
+//need of promises => to avoid callback hell and pyramid of doom problem 
+//promises are used to handle asynchronous operations
+//promises are used to handle errors in a better way
+//promises are used to chain multiple asynchronous operations together
+//promises are used to handle multiple asynchronous operations together
+//promises are used to handle asynchronous operations in a more readable way
