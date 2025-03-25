0d26a966f75f88d612969cfa315d74c271d49385
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-25 21:38:37 +0530

    promises

---
+++ 11_Call_stack/Promises.js
@@ -0,0 +1,14 @@
+function savetoDb(data) {
+    return new Promise((resolve,reject)=>{
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if(internetSpeed>5){
+            resolve("success: data was saved");
+            // resolve(console.log(("success: data was saved")));
+        }else{
+            reject("error: failed to save data");
+            // reject(console.log(("failure: data was not saved")));
+        }
+    });
+  };
+
+  savetoDb("Siddhant Nagaria");
