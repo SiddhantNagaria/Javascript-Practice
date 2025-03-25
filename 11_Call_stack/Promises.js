9c800345e9f4ec84baad91efdd84d94fe26584ee
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-25 22:08:43 +0530

    then catch in promise

--- 11_Call_stack/Promises.js
+++ 11_Call_stack/Promises.js
@@ -1,14 +1,21 @@
 function savetoDb(data) {
-    return new Promise((resolve,reject)=>{
-        let internetSpeed = Math.floor(Math.random() * 10) + 1;
-        if(internetSpeed>5){
-            resolve("success: data was saved");
-            // resolve(console.log(("success: data was saved")));
-        }else{
-            reject("error: failed to save data");
-            // reject(console.log(("failure: data was not saved")));
-        }
-    });
-  };
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 5) {
+      resolve("success: data was saved");
+      // resolve(console.log(("success: data was saved")));
+    } else {
+      reject("error: failed to save data");
+      // reject(console.log(("failure: data was not saved")));
+    }
+  });
+}
 
-  savetoDb("Siddhant Nagaria");
+savetoDb("Siddhant Nagaria")
+  .then(() => {
+    console.log("data was saved");
+  })
+  .catch(() => {
+    console.log("data was not saved");
+  });
+  
