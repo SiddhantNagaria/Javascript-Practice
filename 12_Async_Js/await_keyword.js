3df8cf7ab2e257644e317bc1134c5bc626155637
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-26 20:25:18 +0530

    await

--- 12_Async_Js/await_keyword.js
+++ 12_Async_Js/await_keyword.js
@@ -2,16 +2,23 @@ function getNum() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let num = Math.floor(Math.random() * 10) + 1;
-      console.log(num);
-      resolve();
+      if (num <5) {
+        reject("promise rejected");
+      }
+      console.log("num =", num);
+      resolve("promise resolved");
     }, 1000);
   });
-};
+}
 
 async function demo() {
-  await getNum();
-  await getNum();
-  await getNum();
-};
+  try {
+    await getNum();
+    await getNum();
+    await getNum();
+  } catch{
+    console.log("error caught");
+  }
+}
 
 demo();
