8e7e87994143b92219ecf792db70e1eaf259d210
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-03-26 23:43:21 +0530

    axios headers

--- 12_Async_Js/axios.js
+++ 12_Async_Js/axios.js
@@ -2,7 +2,10 @@ const axios = require("axios");
 const url = "https://api.github.com/users/octocat/repos";
 async function getUrl() {
   try {
-    let result = await axios.get(url);
+    const config = {
+        headers: {Accept:"application/json"}
+    }
+    let result = await axios.get(url, config);
     console.log(result.data);
   } catch (e) {
     console.log("Error = ", e);
